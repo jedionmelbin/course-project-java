fe32c59d88ab25cb37cce58f2ee8621aaa113227
Jedion Melbin <jedionmelbin@gmail.com>
2023-09-07 14:48:59 -0500

    #feat concurrency

---
+++ src/main/java/com/xprotech/app/concurrency/ChapterConcurrency01.java
@@ -0,0 +1,16 @@
+package com.xprotech.app.concurrency;
+
+/**
+ *
+ * @author jedionmelbin
+ */
+public class ChapterConcurrency01 {
+
+    public void tranfer() {
+
+    }
+
+    public void receiver() {
+
+    }
+}
